fe18c77879d9760e7eb6ebd54433c8d5354b455c
Hyun <ghkdgus29@naver.com>
2024-06-27 13:07:58 +0900

    init: 스프링부트 프로젝트 생성

---
+++ src/main/java/com/dowe/DoweApplication.java
@@ -0,0 +1,13 @@
+package com.dowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dowe/DoweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
