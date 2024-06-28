b2a5256dc4cf97e1b31e2d96103b94511d7e7585
yeonise <cerealnum@gmail.com>
2024-06-28 16:20:36 +0900

    feat: RandomUtil 생성

---
+++ src/main/java/com/dowe/util/RandomUtil.java
@@ -0,0 +1,21 @@
+package com.dowe.util;
+
+import java.util.UUID;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class RandomUtil {
+
+	/**
+	 * generate random code
+	 * using a-z A-Z 0-9
+	 */
+	public static String generateRandomCode() {
+		String randomString = UUID.randomUUID().toString().replaceAll("-", "");
+
+		return randomString.substring(0, 10);
+	}
+
+}
