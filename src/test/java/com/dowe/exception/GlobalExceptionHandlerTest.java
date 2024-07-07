d880d1b57fa337abd04b092f2e55589a7d61fc80
yeonise <cerealnum@gmail.com>
2024-07-07 17:37:48 +0900

    fix: Setting active profiles

--- src/test/java/com/dowe/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/com/dowe/exception/GlobalExceptionHandlerTest.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -33,6 +34,7 @@ import com.google.common.base.Charsets;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class GlobalExceptionHandlerTest {
 
 	@Autowired
