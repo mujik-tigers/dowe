c752c401d8ed28f361e0ac5c71b5b086f2d44a28
yeonise <cerealnum@gmail.com>
2024-07-10 23:00:22 +0900

    feat: 토큰 리프레싱을 위한 컨트롤러 구현

--- src/main/java/com/dowe/auth/presentation/AuthController.java
+++ src/main/java/com/dowe/auth/presentation/AuthController.java
@@ -1,13 +1,16 @@
 package com.dowe.auth.presentation;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dowe.auth.application.AuthService;
 import com.dowe.auth.dto.LoginData;
+import com.dowe.auth.dto.TokenPair;
 import com.dowe.member.Provider;
 import com.dowe.util.api.ApiResponse;
 import com.dowe.util.api.ResponseResult;
@@ -24,6 +27,12 @@ public class AuthController {
 	public ResponseEntity<ApiResponse<LoginData>> login(@PathVariable Provider provider, @RequestParam String authorizationCode) {
 		return ResponseEntity.ok()
 			.body(ApiResponse.ok(ResponseResult.LOGIN_SUCCESS, authService.login(provider, authorizationCode)));
+	}
+
+	@GetMapping("/oauth/refresh")
+	public ResponseEntity<ApiResponse<TokenPair>> refresh(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+		return ResponseEntity.ok()
+			.body(ApiResponse.ok(ResponseResult.REFRESH_SUCCESS, authService.refresh(authorizationHeader)));
 	}
 
 }
--- src/main/java/com/dowe/util/api/ResponseResult.java
+++ src/main/java/com/dowe/util/api/ResponseResult.java
@@ -7,6 +7,7 @@ public enum ResponseResult {
 
 	// Auth
 	LOGIN_SUCCESS("로그인을 성공했습니다"),
+	REFRESH_SUCCESS("토큰 리프레싱을 성공했습니다"),
 
 	// Common
 	EXCEPTION_OCCURRED("예외가 발생했습니다");
