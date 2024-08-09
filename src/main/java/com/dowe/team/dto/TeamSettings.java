76ae319e10b853636cea05666a1734d8ed34725b
yeonise <cerealnum@gmail.com>
2024-08-09 17:22:15 +0900

    feat: MultipartFile로 이미지 파일을 받도록 구현

--- src/main/java/com/dowe/team/dto/TeamSettings.java
+++ src/main/java/com/dowe/team/dto/TeamSettings.java
@@ -1,21 +1,25 @@
 package com.dowe.team.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class TeamSettings {
 
-	@NotEmpty
+	@NotEmpty(message = "타이틀은 최소 1자 이상으로 설정해 주세요")
 	@Size(max = 15, message = "타이틀은 최대 15자 이내로 설정해 주세요")
 	private String title;
 
 	@Size(max = 100, message = "설명은 최대 100자 이내로 설정해 주세요")
 	private String description;
 
-	private String image;
+	private MultipartFile image;
 
 }
--- src/main/java/com/dowe/team/presentation/TeamController.java
+++ src/main/java/com/dowe/team/presentation/TeamController.java
@@ -2,8 +2,8 @@ package com.dowe.team.presentation;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dowe.team.application.TeamService;
@@ -23,7 +23,7 @@ public class TeamController {
 	private final TeamService teamService;
 
 	@PostMapping("/teams")
-	public ResponseEntity<ApiResponse<NewTeam>> create(@Login Long memberId, @RequestBody @Valid TeamSettings teamSettings) {
+	public ResponseEntity<ApiResponse<NewTeam>> create(@Login Long memberId, @ModelAttribute @Valid TeamSettings teamSettings) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 			.body(ApiResponse.created(ResponseResult.TEAM_CREATE_SUCCESS, teamService.create(memberId, teamSettings)));
 	}
