832a9857351c2a06ed0ed52be3d5ef316cde3b5f
yeonise <cerealnum@gmail.com>
2024-08-09 17:27:03 +0900

    feat: 팀 생성을 위한 Service 로직 구현

--- src/main/java/com/dowe/team/application/TeamService.java
+++ src/main/java/com/dowe/team/application/TeamService.java
@@ -12,6 +12,7 @@ import com.dowe.team.Team;
 import com.dowe.team.dto.NewTeam;
 import com.dowe.team.dto.TeamSettings;
 import com.dowe.team.infrastructure.TeamRepository;
+import com.dowe.util.S3Uploader;
 import com.dowe.util.StringUtil;
 
 import lombok.RequiredArgsConstructor;
@@ -20,17 +21,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TeamService {
 
+	private static final String TEAM_IMAGE_DIRECTORY = "team";
+
 	private final TeamRepository teamRepository;
 	private final MemberRepository memberRepository;
 	private final ProfileRepository profileRepository;
+	private final S3Uploader s3Uploader;
 
 	@Transactional
 	public NewTeam create(Long memberId, TeamSettings teamSettings) {
 		Member member = memberRepository.findById(memberId)
 			.orElseThrow(MemberNotFoundException::new);
 
+		String image = null;
+		if (teamSettings.getImage() != null && !teamSettings.getImage().isEmpty()) {
+			image = s3Uploader.upload(TEAM_IMAGE_DIRECTORY, teamSettings.getImage());
+		}
+
 		Team teamBuilder = Team.builder()
 			.title(StringUtil.removeExtraSpaces(teamSettings.getTitle()))
+			.description(StringUtil.removeExtraSpaces(teamSettings.getDescription()))
+			.image(image)
 			.manager(member)
 			.build();
 
@@ -38,7 +49,7 @@ public class TeamService {
 		Profile profile = team.join(member);
 		profileRepository.save(profile);
 
-		return new NewTeam(team, profile);
+		return new NewTeam(team);
 	}
 
 }
--- src/main/java/com/dowe/team/dto/NewTeam.java
+++ src/main/java/com/dowe/team/dto/NewTeam.java
@@ -1,6 +1,5 @@
 package com.dowe.team.dto;
 
-import com.dowe.profile.Profile;
 import com.dowe.team.Team;
 
 import lombok.Getter;
@@ -9,15 +8,9 @@ import lombok.Getter;
 public class NewTeam {
 
 	private final Long teamId;
-	private final String teamTitle;
-	private final String teamDescription;
-	private final String teamImage;
 
-	public NewTeam(Team team, Profile profile) {
+	public NewTeam(Team team) {
 		this.teamId = team.getId();
-		this.teamTitle = team.getTitle();
-		this.teamDescription = team.getDescription();
-		this.teamImage = team.getImage();
 	}
 
 }
