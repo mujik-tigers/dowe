4210dbdd08f2f4aad410e5854949bdd4f9344d3e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-08-25 22:21:59 +0900

    feat: add necessary annotations in `TodoList.java`

--- src/main/java/com/dowe/todolist/TodoList.java
+++ src/main/java/com/dowe/todolist/TodoList.java
@@ -1,13 +1,35 @@
 package com.dowe.todolist;
 
+import com.dowe.profile.Profile;
+import com.dowe.util.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TodoList extends BaseEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@ManyToOne
+	@JoinColumn(name = "profile_id")
 	private Profile profile;
 
 	private boolean completed;
 
+	@Builder
 	private TodoList(
 		Profile profile
 	) {
