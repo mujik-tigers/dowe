26c11c3190969e412f85f89a81b493c13ae52d38
yeonise <cerealnum@gmail.com>
2024-08-26 18:08:26 +0900

    feat: create DTO `TeamInList`

---
+++ src/main/java/com/dowe/member/dto/TeamInList.java
@@ -0,0 +1,22 @@
+package com.dowe.member.dto;
+
+import com.dowe.team.Team;
+
+import lombok.Getter;
+
+@Getter
+public class TeamInList {
+
+	private final Long id;
+	private final String title;
+	private final String description;
+	private final String image;
+
+	public TeamInList(Team team) {
+		this.id = team.getId();
+		this.title = team.getTitle();
+		this.description = team.getDescription();
+		this.image = team.getImage();
+	}
+
+}
