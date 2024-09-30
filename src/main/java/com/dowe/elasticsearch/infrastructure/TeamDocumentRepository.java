cb8c1ad075e3ee251aed832607c034a821284d4a
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-09-30 20:19:51 +0900

    refactor: rename `findByTeamName()` to `findByTeamTitle()` in `TeamDocumentRepository.java`

--- src/main/java/com/dowe/elasticsearch/infrastructure/TeamDocumentRepository.java
+++ src/main/java/com/dowe/elasticsearch/infrastructure/TeamDocumentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 public interface TeamDocumentRepository extends ElasticsearchRepository<TeamDocument, Long> {
 
-  List<TeamDocument> findByTeamName(String teamName);
+  List<TeamDocument> findByTeamTitle(String teamTitle);
 }
