301287b2c9395dbfd079d406145fc046be7787fb
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-09-30 18:55:44 +0900

    feat: add `findByTeamName()` in `TeamDocumentRepository.java`

--- src/main/java/com/dowe/elasticsearch/infrastructure/TeamDocumentRepository.java
+++ src/main/java/com/dowe/elasticsearch/infrastructure/TeamDocumentRepository.java
@@ -1,8 +1,10 @@
 package com.dowe.elasticsearch.infrastructure;
 
 import com.dowe.elasticsearch.document.TeamDocument;
+import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface TeamDocumentRepository extends ElasticsearchRepository<TeamDocument, Long> {
 
+  List<TeamDocument> findByTeamName(String teamName);
 }
