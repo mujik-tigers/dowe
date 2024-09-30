1bbf8b833e312c23ce6cb4be09ebe1cb2a6607e6
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-09-30 18:50:49 +0900

    feat: add necessary annotations in `TeamDocument.java`

--- src/main/java/com/dowe/elasticsearch/document/TeamDocument.java
+++ src/main/java/com/dowe/elasticsearch/document/TeamDocument.java
@@ -1,7 +1,14 @@
 package com.dowe.elasticsearch.document;
 
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Getter
+@Document(indexName = "team")
 public class TeamDocument {
 
+  @Id
   private String id;
   private String title;
 
