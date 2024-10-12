301013153a5fb4373b5679fa787ea4c69903d82a
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-12 15:44:44 +0900

    refactor: edit constructor parameters in `TeamOutline.java`

--- src/main/java/com/dowe/team/dto/TeamOutline.java
+++ src/main/java/com/dowe/team/dto/TeamOutline.java
@@ -2,26 +2,27 @@ package com.dowe.team.dto;
 
 import static com.dowe.util.AppConstants.*;
 
-import com.dowe.team.Team;
+import com.dowe.elasticsearch.document.TeamDocument;
 
 public record TeamOutline(
-	Long id,
-	String title,
-	String image,
-	int currentPeople,
-	int maxPeople
+    Long id,
+    String title,
+    String image,
+    int currentPeople,
+    int maxPeople
 ) {
 
-	public static TeamOutline of(
-		Team team
-	) {
-		return new TeamOutline(
-			team.getId(),
-			team.getTitle(),
-			team.getImage(),
-			team.getProfiles().size(),
-			TEAM_MAX_SIZE
-		);
-	}
+  public static TeamOutline of(
+      TeamDocument teamDocument,
+      int currentPeople
+  ) {
+    return new TeamOutline(
+        teamDocument.getId(),
+        teamDocument.getTitle(),
+        teamDocument.getImage(),
+        currentPeople,
+        TEAM_MAX_SIZE
+    );
+  }
 
 }
