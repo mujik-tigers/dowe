59f125ecd8b39b7046f7354a5afac5e374c845b8
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-12 12:25:37 +0900

    feat: add fields in `FindByTeamTitleResponse.java`

--- src/main/java/com/dowe/elasticsearch/dto/response/FindByTeamTitleResponse.java
+++ src/main/java/com/dowe/elasticsearch/dto/response/FindByTeamTitleResponse.java
@@ -4,6 +4,9 @@ import com.dowe.team.dto.TeamOutline;
 import java.util.List;
 
 public record FindByTeamTitleResponse(
+    boolean hasMore,
+    Long lastUnixTimestamp,
+    String lastTieBreakerId,
     List<TeamOutline> teamOutlineList
 ) {
 
