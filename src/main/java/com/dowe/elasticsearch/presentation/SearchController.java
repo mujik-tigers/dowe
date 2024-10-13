04af0bce1f35b8f9f853f460cfbfaf5acd3daa21
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-13 17:25:54 +0900

    feat: add `searchTeamsByTitle()` in `SearchController.java`

--- src/main/java/com/dowe/elasticsearch/presentation/SearchController.java
+++ src/main/java/com/dowe/elasticsearch/presentation/SearchController.java
@@ -1,8 +1,15 @@
 package com.dowe.elasticsearch.presentation;
 
 import com.dowe.elasticsearch.application.SearchService;
+import com.dowe.elasticsearch.dto.response.SearchByTeamTitleResponse;
+import com.dowe.util.api.ApiResponse;
+import com.dowe.util.api.ResponseResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchController {
 
   private final SearchService searchService;
+
+  @GetMapping("/teams")
+  public ResponseEntity<ApiResponse<SearchByTeamTitleResponse>> searchTeamsByTitle(
+      @RequestParam(required = true) String title,
+      @RequestParam(defaultValue = "10") int size,
+      @RequestParam(defaultValue = "0") Long lastUnixTimestamp,
+      @RequestParam(defaultValue = "0") String lastTieBreakerId
+  ) {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(ApiResponse.ok(
+                ResponseResult.TEAM_FIND_SUCCESS,
+                searchService.searchByTeamTitle(
+                    size,
+                    title,
+                    lastUnixTimestamp,
+                    lastTieBreakerId
+                )
+            )
+        );
+  }
 
 }
