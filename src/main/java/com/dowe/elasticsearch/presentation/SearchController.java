7f7abcd6ddca78ca071d82a48fabdbce7397bbfb
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-13 17:22:27 +0900

    feat: add necessary annotations in `SearchController.java`

--- src/main/java/com/dowe/elasticsearch/presentation/SearchController.java
+++ src/main/java/com/dowe/elasticsearch/presentation/SearchController.java
@@ -1,6 +1,12 @@
 package com.dowe.elasticsearch.presentation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/search")
 public class SearchController {
 
 }
