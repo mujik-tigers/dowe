ac885b0985f531934a8ee8f8f697f9b18041faf8
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-15 20:58:21 +0900

    refactor: remove unnecessary code in `TeamOutline.java`

--- src/main/java/com/dowe/team/dto/TeamOutline.java
+++ src/main/java/com/dowe/team/dto/TeamOutline.java
@@ -1,10 +1,5 @@
 package com.dowe.team.dto;
 
-import static com.dowe.util.AppConstants.*;
-
-import com.dowe.elasticsearch.document.TeamDocument;
-import com.dowe.team.Team;
-
 public record TeamOutline(
     Long id,
     String title,
@@ -12,30 +7,5 @@ public record TeamOutline(
     int currentPeople,
     int maxPeople
 ) {
-
-  public static TeamOutline of(
-      TeamDocument teamDocument,
-      int currentPeople
-  ) {
-    return new TeamOutline(
-        teamDocument.getId(),
-        teamDocument.getTitle(),
-        teamDocument.getImage(),
-        currentPeople,
-        TEAM_MAX_SIZE
-    );
-  }
-
-  public static TeamOutline of(
-      Team team
-  ) {
-    return new TeamOutline(
-        team.getId(),
-        team.getTitle(),
-        team.getImage(),
-        team.getProfiles().size(),
-        TEAM_MAX_SIZE
-    );
-  }
 
 }
