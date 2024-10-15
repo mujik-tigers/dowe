1adf76893a6e708109818842ff1a920cdbe9ed0c
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-15 20:58:35 +0900

    feat: add necessary fields in `TeamDocumentOutline.java`

--- src/main/java/com/dowe/team/dto/TeamDocumentOutline.java
+++ src/main/java/com/dowe/team/dto/TeamDocumentOutline.java
@@ -1,5 +1,12 @@
 package com.dowe.team.dto;
 
-public record TeamDocumentOutline() {
+public record TeamDocumentOutline(
+    Long id,
+    String title,
+    String description,
+    String image,
+    int currentPeople,
+    int maxPeople
+) {
 
 }
