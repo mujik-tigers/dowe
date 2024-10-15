41bfd048feaa11fbf1b8fb7cb3a71fbc6d6ac0ce
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-15 20:26:52 +0900

    refactor: modify `TeamMapper.java` to interface for MapStruct usage

--- src/main/java/com/dowe/elasticsearch/mapper/TeamMapper.java
+++ src/main/java/com/dowe/elasticsearch/mapper/TeamMapper.java
@@ -1,24 +1,5 @@
 package com.dowe.elasticsearch.mapper;
 
-import static com.dowe.util.AppConstants.*;
-
-import com.dowe.elasticsearch.document.TeamDocument;
-import com.dowe.team.dto.TeamOutline;
-import org.springframework.stereotype.Component;
-
-@Component
-public class TeamMapper {
-
-  public TeamOutline toTeamOutline(
-      TeamDocument teamDocument
-  ) {
-    return new TeamOutline(
-        teamDocument.getId(),
-        teamDocument.getTitle(),
-        teamDocument.getDescription(),
-        teamDocument.getMemberCount(),
-        TEAM_MAX_SIZE
-    );
-  }
+public interface TeamMapper {
 
 }
