bcd123aa5db58c17ecbcc42672da6d1e439786d4
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-15 20:59:18 +0900

    feat: add `toTeamOutline()` in `TeamMapper.java`

--- src/main/java/com/dowe/elasticsearch/mapper/TeamMapper.java
+++ src/main/java/com/dowe/elasticsearch/mapper/TeamMapper.java
@@ -1,8 +1,15 @@
 package com.dowe.elasticsearch.mapper;
 
+import com.dowe.team.Team;
+import com.dowe.team.dto.TeamOutline;
+import com.dowe.util.AppConstants;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring", imports = AppConstants.class)
 public interface TeamMapper {
+
+  @Mapping(target = "maxPeople", expression = "java(AppConstants.TEAM_MAX_SIZE)")
+  TeamOutline toTeamOutline(Team team);
 
 }
