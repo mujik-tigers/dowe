4cef7f679d6f3ad078661eea2e834ecdde16a222
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-15 20:24:17 +0900

    feat: add mapping method in `TeamDocumentMapper.java`

--- src/main/java/com/dowe/elasticsearch/mapper/TeamDocumentMapper.java
+++ src/main/java/com/dowe/elasticsearch/mapper/TeamDocumentMapper.java
@@ -1,8 +1,15 @@
 package com.dowe.elasticsearch.mapper;
 
+import com.dowe.elasticsearch.document.TeamDocument;
+import com.dowe.team.dto.TeamDocumentOutline;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface TeamDocumentMapper {
+
+  TeamDocumentMapper INSTANCE = Mappers.getMapper(TeamDocumentMapper.class);
+
+  TeamDocumentOutline toTeamDocumentOutline(TeamDocument teamDocument);
 
 }
