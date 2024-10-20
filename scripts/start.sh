71503c8dc1395c4dea1e9040c8e193a194d8c71c
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-20 14:33:06 +0900

    fix: edit `start.sh`

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/dowe.jar"
+SHORT_JAR_FILE="/dowe.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -9,13 +10,15 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+echo "$TIME_NOW > start.sh 스크립트 실행" >> $DEPLOY_LOG
+
 # build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+echo "$TIME_NOW > $FULL_JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+CURRENT_PID=$(pgrep -f $SHORT_JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디: $CURRENT_PID" >> $DEPLOY_LOG
