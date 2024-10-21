99c27b71a85acb5092dfbea4a1c5c326ab5b40e2
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-21 22:49:17 +0900

    feat: add `origin` variable in `OAuthProvider.java`

--- src/main/java/com/dowe/auth/application/OAuthProvider.java
+++ src/main/java/com/dowe/auth/application/OAuthProvider.java
@@ -2,6 +2,7 @@ package com.dowe.auth.application;
 
 import static com.dowe.util.AppConstants.*;
 
+import com.dowe.util.resolver.RedirectUriResolver;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
@@ -23,45 +24,59 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class OAuthProvider {
 
-	private final OAuthProperties properties;
-	private final RestClient restClient;
+  private final OAuthProperties properties;
+  private final RestClient restClient;
+  private final RedirectUriResolver redirectUriResolver;
 
-	public String authenticate(Provider provider, String authorizationCode) {
-		AccessToken accessToken = requestAccessToken(provider, authorizationCode);
-		UserResource userResource = requestUserId(provider, accessToken.getAccessToken());
 
-		return userResource.getId();
-	}
+  public String authenticate(
+      String origin,
+      Provider provider,
+      String authorizationCode
+  ) {
+    AccessToken accessToken = requestAccessToken(
+        origin,
+        provider,
+        authorizationCode
+    );
+    UserResource userResource = requestUserId(provider, accessToken.getAccessToken());
 
-	private AccessToken requestAccessToken(Provider provider, String authorizationCode) {
-		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-		body.add("grant_type", AUTHORIZATION_CODE);
-		body.add("client_id", properties.getClientIdOf(provider));
-		body.add("redirect_uri", properties.getRedirectUriOf(provider));
-		body.add("code", authorizationCode);
-		body.add("client_secret", properties.getClientSecretOf(provider));
+    return userResource.getId();
+  }
 
-		return restClient.post()
-			.uri(properties.getTokenRequestUriOf(provider))
-			.header(HttpHeaders.CONTENT_TYPE, X_WWW_FORM_URLENCODED_CHARSET_UTF_8)
-			.body(body)
-			.retrieve()
-			.onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
-				log.info("Request Access Token Failed : status={}", response.getStatusText());
-				throw new InvalidAuthorizationCodeException();
-			})
-			.toEntity(AccessToken.class)
-			.getBody();
-	}
+  private AccessToken requestAccessToken(
+      String origin,
+      Provider provider,
+      String authorizationCode
+  ) {
+    MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+    body.add("grant_type", AUTHORIZATION_CODE);
+    body.add("client_id", properties.getClientIdOf(provider));
+    body.add("redirect_uri", redirectUriResolver.resolvedRedirectUri(origin, provider));
+    body.add("code", authorizationCode);
+    body.add("client_secret", properties.getClientSecretOf(provider));
 
-	private UserResource requestUserId(Provider provider, String accessToken) {
-		return restClient.get()
-			.uri(properties.getResourceRequestUriOf(provider))
-			.header(HttpHeaders.AUTHORIZATION, BEARER + accessToken)
-			.header(HttpHeaders.CONTENT_TYPE, X_WWW_FORM_URLENCODED_CHARSET_UTF_8)
-			.retrieve()
-			.toEntity(UserResource.class)
-			.getBody();
-	}
+    return restClient.post()
+        .uri(properties.getTokenRequestUriOf(provider))
+        .header(HttpHeaders.CONTENT_TYPE, X_WWW_FORM_URLENCODED_CHARSET_UTF_8)
+        .body(body)
+        .retrieve()
+        .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+          log.info("Request Access Token Failed : status={}", response.getStatusText());
+          throw new InvalidAuthorizationCodeException();
+        })
+        .toEntity(AccessToken.class)
+        .getBody();
+  }
+
+  private UserResource requestUserId(Provider provider, String accessToken) {
+    return restClient.get()
+        .uri(properties.getResourceRequestUriOf(provider))
+        .header(HttpHeaders.AUTHORIZATION, BEARER + accessToken)
+        .header(HttpHeaders.CONTENT_TYPE, X_WWW_FORM_URLENCODED_CHARSET_UTF_8)
+        .retrieve()
+        .toEntity(UserResource.class)
+        .getBody();
+  }
 
 }
