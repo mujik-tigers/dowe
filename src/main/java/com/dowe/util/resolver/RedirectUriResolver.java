cdef635b6759eb9405af9bd4a0fca2e8b7ca1cce
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-21 22:48:38 +0900

    feat: add `RedirectUriResolver.java`

---
+++ src/main/java/com/dowe/util/resolver/RedirectUriResolver.java
@@ -0,0 +1,30 @@
+package com.dowe.util.resolver;
+
+import static com.dowe.util.AppConstants.*;
+
+import com.dowe.config.properties.OAuthProperties;
+import com.dowe.member.Provider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedirectUriResolver {
+
+  private final OAuthProperties oAuthProperties;
+
+  public String resolvedRedirectUri(
+      String origin,
+      Provider provider
+  ) {
+
+    String baseUri = oAuthProperties.getRedirectBaseUriOf(provider);
+
+    if (origin != null && oAuthProperties.getAllowedOrigins().contains(origin)) {
+      return origin + baseUri;
+    }
+    return DOMAIN + baseUri;
+  }
+
+}
