b48951dda35bbac915abc55b76c3f399e19865a7
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-28 11:20:27 +0900

    feat: add `TestConstants.java`

---
+++ src/test/java/com/dowe/TestConstants.java
@@ -0,0 +1,22 @@
+package com.dowe;
+
+public final class TestConstants {
+
+  public static final String BEARER = "Bearer ";
+  public static final String AUTHORIZATION = "Authorization";
+  public static final String ORIGIN = "Origin";
+  public static final String HOST = "Host";
+  public static final String CONTENT_TYPE = "Content-Type";
+  public static final String X_WWW_FORM_URLENCODED_CHARSET_UTF_8 = "application/x-www-form-urlencoded;charset=utf-8";
+
+  public static final String MEMBER_ID = "memberId";
+  public static final String TOKEN_TYPE = "tokenType";
+  public static final String MEMBER_CODE_SET = "memberCode";
+
+
+  public static final String FRONTEND_DOMAIN = "https://dowith.today";
+  public static final String BACKEND_DOMAIN = "api.dowith.today";
+
+  public static final int TEAM_MAX_SIZE = 10;
+
+}
