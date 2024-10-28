27bc8d4bbd656b4f73dc450a9fc462a646cbb1ff
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-28 12:31:40 +0900

    feat: add `setUp()` in `RestDocsSupport.java`

--- src/test/java/com/dowe/RestDocsSupport.java
+++ src/test/java/com/dowe/RestDocsSupport.java
@@ -1,12 +1,19 @@
 package com.dowe;
 
+import com.dowe.config.RestDocsConfig;
 import com.dowe.elasticsearch.application.SearchService;
 import com.dowe.elasticsearch.presentation.SearchController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.dowe.auth.application.AuthService;
@@ -19,6 +26,10 @@ import com.dowe.team.presentation.TeamController;
 import com.dowe.util.interceptor.AccessTokenInterceptor;
 import com.dowe.util.interceptor.AuthorizationHeaderInterceptor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @WebMvcTest(controllers = {
     AuthController.class,
@@ -26,7 +37,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
     TeamController.class,
     SearchController.class
 })
-@AutoConfigureRestDocs
+@Import(RestDocsConfig.class)
+@ExtendWith(RestDocumentationExtension.class)
 public abstract class RestDocsSupport {
 
   @Autowired
@@ -34,6 +46,9 @@ public abstract class RestDocsSupport {
 
   @Autowired
   protected ObjectMapper objectMapper;
+
+  @Autowired
+  protected RestDocumentationResultHandler restDocs;
 
   @SpyBean
   protected AccessTokenInterceptor accessTokenInterceptor;
@@ -55,5 +70,20 @@ public abstract class RestDocsSupport {
 
   @MockBean
   protected SearchService searchService;
+
+  @BeforeEach
+  void setUp(
+      final WebApplicationContext context,
+      final RestDocumentationContextProvider provider
+  ) {
+    this.mockMvc = MockMvcBuilders
+        .webAppContextSetup(context)
+        .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
+        .alwaysDo(MockMvcResultHandlers.print())
+        .alwaysDo(restDocs)
+        .addFilters(new CharacterEncodingFilter("UTF-8", true))
+        .build();
+  }
+
 
 }
