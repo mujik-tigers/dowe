74c777028720f1df32decc05b00b96e4c46cbffa
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-10-28 12:33:14 +0900

    feat: add `write()` in `RestDocsConfig.java`

--- src/test/java/com/dowe/config/RestDocsConfig.java
+++ src/test/java/com/dowe/config/RestDocsConfig.java
@@ -1,4 +1,20 @@
 package com.dowe.config;
 
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+
 public class RestDocsConfig {
+
+  @Bean
+  public RestDocumentationResultHandler write() {
+    return MockMvcRestDocumentation.document(
+        "{class-name}/{method-name}",
+        preprocessRequest(prettyPrint()),
+        preprocessResponse(prettyPrint())
+    );
+  }
+
 }
