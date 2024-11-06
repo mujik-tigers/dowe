b3aca9915671fc7f6253284a0bcda5f05ac47805
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-06 21:36:22 +0900

    feat: add static factory methods in `SearchTeamsByTitleResponse.java`

--- src/main/java/com/dowe/elasticsearch/dto/response/SearchTeamsByTitleResponse.java
+++ src/main/java/com/dowe/elasticsearch/dto/response/SearchTeamsByTitleResponse.java
@@ -1,5 +1,8 @@
 package com.dowe.elasticsearch.dto.response;
 
+import static com.dowe.util.AppConstants.LAST_TIE_BREAKER_ID_INDEX;
+import static com.dowe.util.AppConstants.LAST_UNIX_TIMESTAMP_INDEX;
+
 import com.dowe.team.dto.TeamDocumentOutline;
 import java.util.List;
 
@@ -9,5 +12,27 @@ public record SearchTeamsByTitleResponse(
     Long lastTieBreakerId,
     List<TeamDocumentOutline> teamDocumentOutlines
 ) {
+
+  public static SearchTeamsByTitleResponse empty() {
+    return new SearchTeamsByTitleResponse(
+        false,
+        0L,
+        0L,
+        List.of()
+    );
+  }
+
+  public static SearchTeamsByTitleResponse of(
+      boolean hasMore,
+      List<Object> lastSortValues,
+      List<TeamDocumentOutline> teamDocumentOutlines
+  ) {
+    return new SearchTeamsByTitleResponse(
+        hasMore,
+        (Long) lastSortValues.get(LAST_UNIX_TIMESTAMP_INDEX),
+        (Long) lastSortValues.get(LAST_TIE_BREAKER_ID_INDEX),
+        teamDocumentOutlines
+    );
+  }
 
 }
