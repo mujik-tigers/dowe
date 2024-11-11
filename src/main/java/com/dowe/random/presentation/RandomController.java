4ddbaee708e9820b247fc1dbeefade006c553849
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-11 17:56:49 +0900

    feat: add necessary annotations and fields in `RandomController.java`

--- src/main/java/com/dowe/random/presentation/RandomController.java
+++ src/main/java/com/dowe/random/presentation/RandomController.java
@@ -1,5 +1,15 @@
 package com.dowe.random.presentation;
 
+import com.dowe.random.application.RandomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/random")
 public class RandomController {
+
+  private final RandomService randomService;
 
 }
