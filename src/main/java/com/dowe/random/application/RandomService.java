efd20611e234e18cbfed72e926239630ebb52d39
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-11 17:56:04 +0900

    feat: add `getRandomTeams()` in `RandomService.java`

--- src/main/java/com/dowe/random/application/RandomService.java
+++ src/main/java/com/dowe/random/application/RandomService.java
@@ -1,8 +1,16 @@
 package com.dowe.random.application;
 
+import static com.dowe.util.AppConstants.*;
+
 import com.dowe.elasticsearch.mapper.TeamMapper;
+import com.dowe.exception.member.MemberNotFoundException;
+import com.dowe.member.Member;
 import com.dowe.member.infrastructure.MemberRepository;
+import com.dowe.random.dto.response.RandomTeamsResponse;
+import com.dowe.team.dto.TeamOutline;
 import com.dowe.team.infrastructure.TeamRepository;
+import java.util.List;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,5 +23,26 @@ public class RandomService {
   private final TeamRepository teamRepository;
   private final MemberRepository memberRepository;
   private final TeamMapper teamMapper;
+
+  public RandomTeamsResponse getRandomTeams(
+      Long memberId
+  ) {
+    Member member = memberRepository.findById(memberId)
+        .orElseThrow(MemberNotFoundException::new);
+
+    int memberNotJoinedTeamsCount = teamRepository.countTeamsNotJoinedByMember(member.getId());
+
+    int maxOffset = Math.max(0, memberNotJoinedTeamsCount - RANDOM_TEAMS_SIZE);
+
+    int randomOffset = new Random().nextInt(maxOffset + 1);
+
+    List<TeamOutline> randomTeamOutlines = teamRepository.findTeamsNotJoinedByMemberWithOffset(memberId, randomOffset)
+        .stream()
+        .map(teamMapper::toTeamOutline)
+        .toList();
+
+    return RandomTeamsResponse.from(randomTeamOutlines);
+
+  }
 
 }
