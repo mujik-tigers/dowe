140dab0ae4a898d1fb6c080b0546a0daaff7d38b
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-11 17:55:48 +0900

    feat: add necessary annotations and fields in `RandomService.java`

--- src/main/java/com/dowe/random/application/RandomService.java
+++ src/main/java/com/dowe/random/application/RandomService.java
@@ -1,5 +1,19 @@
 package com.dowe.random.application;
 
+import com.dowe.elasticsearch.mapper.TeamMapper;
+import com.dowe.member.infrastructure.MemberRepository;
+import com.dowe.team.infrastructure.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class RandomService {
+
+  private final TeamRepository teamRepository;
+  private final MemberRepository memberRepository;
+  private final TeamMapper teamMapper;
 
 }
