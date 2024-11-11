b4f6e570ca63c6970c0fc355004af547e18c144b
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-11 17:53:32 +0900

    feat: add parameter and static factory method in `RandomTeamsResponse.java`

--- src/main/java/com/dowe/random/dto/response/RandomTeamsResponse.java
+++ src/main/java/com/dowe/random/dto/response/RandomTeamsResponse.java
@@ -1,5 +1,19 @@
 package com.dowe.random.dto.response;
 
-public record RandomTeamsResponse() {
+import com.dowe.team.dto.TeamOutline;
+import java.util.List;
+
+public record RandomTeamsResponse(
+    List<TeamOutline> randomTeamOutlines
+) {
+
+  public static RandomTeamsResponse from(
+      List<TeamOutline> randomTeamOutlines
+  ) {
+    return new RandomTeamsResponse(
+        randomTeamOutlines
+    );
+  }
+
 
 }
