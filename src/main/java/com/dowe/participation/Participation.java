f2ff3c3ab2a5c15711fbb976ead4152d74bcf06e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-24 18:11:53 +0900

    feat: add necessary fields and annotations in `Participation.java`

--- src/main/java/com/dowe/participation/Participation.java
+++ src/main/java/com/dowe/participation/Participation.java
@@ -1,6 +1,15 @@
 package com.dowe.participation;
 
+import com.dowe.member.Member;
+import com.dowe.team.Team;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,5 +17,17 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Participation {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "team_id")
+  private Team team;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
 
 }
