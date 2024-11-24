c301f5c7bc14900cbd86855a6c730a18e387a80a
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-24 16:51:44 +0900

    feat: add necessary field in `ProfileService.java`

--- src/main/java/com/dowe/profile/application/ProfileService.java
+++ src/main/java/com/dowe/profile/application/ProfileService.java
@@ -1,10 +1,13 @@
 package com.dowe.profile.application;
 
+import com.dowe.profile.infrastructure.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
+
+  private final ProfileRepository profileRepository;
 
 }
