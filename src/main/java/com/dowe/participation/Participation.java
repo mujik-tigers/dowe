8deb47cea7778810684a4f18aeb7a34fc13c853e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-24 18:11:35 +0900

    feat: add necessary annotations in `Participation.java`

--- src/main/java/com/dowe/participation/Participation.java
+++ src/main/java/com/dowe/participation/Participation.java
@@ -1,5 +1,12 @@
 package com.dowe.participation;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Participation {
 
 }
