350f2533b2bfdde3ab2c81e8926eedb88d9b14d1
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-24 16:51:08 +0900

    feat: add necessary annotations in `ProfileService.java`

--- src/main/java/com/dowe/profile/application/ProfileService.java
+++ src/main/java/com/dowe/profile/application/ProfileService.java
@@ -1,5 +1,10 @@
 package com.dowe.profile.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProfileService {
 
 }
