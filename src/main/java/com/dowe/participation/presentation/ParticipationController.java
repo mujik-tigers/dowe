c9dfd4e33497bf8f8ee5498bb890f3e92e761a75
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-25 02:27:16 +0900

    feat: add necessary field in `ParticipationController.java`

--- src/main/java/com/dowe/participation/presentation/ParticipationController.java
+++ src/main/java/com/dowe/participation/presentation/ParticipationController.java
@@ -1,5 +1,7 @@
 package com.dowe.participation.presentation;
 
+
+import com.dowe.participation.application.ParticipationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ParticipationController {
 
+  private final ParticipationService participationService;
 
 
 }
