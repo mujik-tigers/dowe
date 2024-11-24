8acbc6985de28b3da75bdbfa4658df86883cbad9
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-25 02:27:53 +0900

    feat: add `getMyPendingRequests()` in `ParticipationService.java`

--- src/main/java/com/dowe/participation/application/ParticipationService.java
+++ src/main/java/com/dowe/participation/application/ParticipationService.java
@@ -1,6 +1,12 @@
 package com.dowe.participation.application;
 
+import static com.dowe.participation.ParticipationRequestStatus.*;
+
+import com.dowe.participation.Participation;
+import com.dowe.participation.dto.response.GetMyPendingRequestsResponse;
 import com.dowe.participation.infrastructure.ParticipationRepository;
+import com.dowe.participation.dto.PendingRequestTeamInfo;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,5 +17,25 @@ import org.springframework.stereotype.Service;
 public class ParticipationService {
 
   private final ParticipationRepository participationRepository;
+
+  public GetMyPendingRequestsResponse getMyPendingRequests(
+      Long memberId
+  ) {
+    log.info(">>> ParticipationService getMyPendingRequests()");
+
+    List<Participation> myPendingRequests = participationRepository.findWithTeamByMemberIdAndStatus(
+        memberId,
+        PENDING
+    );
+
+    List<PendingRequestTeamInfo> myPendingRequestsTeamInfos = myPendingRequests.stream()
+        .map(participation -> new PendingRequestTeamInfo(
+            participation.getTeam().getTitle(),
+            participation.getTeam().getImage()
+        ))
+        .toList();
+
+    return new GetMyPendingRequestsResponse(myPendingRequestsTeamInfos);
+  }
 
 }
