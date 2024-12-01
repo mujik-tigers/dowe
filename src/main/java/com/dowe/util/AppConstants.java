931065ade6d711ea080a600d710a67cb63c9b85c
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-12-01 13:48:40 +0900

    feat: add `MAXIMUM_TEAM_COUNT` in `AppConstants.java`

--- src/main/java/com/dowe/util/AppConstants.java
+++ src/main/java/com/dowe/util/AppConstants.java
@@ -10,6 +10,7 @@ public final class AppConstants {
   public static final String X_WWW_FORM_URLENCODED_CHARSET_UTF_8 = "application/x-www-form-urlencoded;charset=utf-8";
 
   public static final String MEMBER_ID = "memberId";
+  public static final int MAXIMUM_TEAM_COUNT = 5;
   public static final String AUTHORIZATION_CODE = "authorization_code";
   public static final String TOKEN_TYPE = "tokenType";
   public static final String TEAM_TITLE = "title";
