430de830e287ad7ea32dc705210295e8687adf20
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-12-02 18:30:26 +0900

    feat: add necessary fields and annotations in `S3PresignedUrlGenerator.java`

--- src/main/java/com/dowe/util/s3/S3PresignedUrlGenerator.java
+++ src/main/java/com/dowe/util/s3/S3PresignedUrlGenerator.java
@@ -1,10 +1,17 @@
 package com.dowe.util.s3;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Component
 @RequiredArgsConstructor
 public class S3PresignedUrlGenerator {
+
+  private final S3Presigner s3Presigner;
+
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucketName;
 
 }
