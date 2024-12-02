dda236b41a3b1e96c3ded71047649f113b036323
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-12-02 18:29:44 +0900

    feat: add necessary annotations in `S3PresignedUrlGenerator.java`

--- src/main/java/com/dowe/util/s3/S3PresignedUrlGenerator.java
+++ src/main/java/com/dowe/util/s3/S3PresignedUrlGenerator.java
@@ -1,5 +1,10 @@
 package com.dowe.util.s3;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class S3PresignedUrlGenerator {
 
 }
